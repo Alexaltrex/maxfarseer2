f569189b78254a2d38f91502fb9aed78a23d7ff0
Alexaltrex <alexaltrex@mail.ru>
2020-07-28 04:21:24 +0400

    Updates

---
+++ precache-manifest.1de74c60bb3af40da77b29688dd2076e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a0063b12b4191a647bb91004ce9bb3",
+    "url": "/maxfarseer2/index.html"
+  },
+  {
+    "revision": "0f641207248edc211d19",
+    "url": "/maxfarseer2/static/css/main.09bbca36.chunk.css"
+  },
+  {
+    "revision": "446d8317863e85e37d02",
+    "url": "/maxfarseer2/static/js/2.58e0eb9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/maxfarseer2/static/js/2.58e0eb9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f641207248edc211d19",
+    "url": "/maxfarseer2/static/js/main.70e8e24f.chunk.js"
+  },
+  {
+    "revision": "6b5040d550cd34664fad",
+    "url": "/maxfarseer2/static/js/runtime-main.a3ae2547.js"
+  },
+  {
+    "revision": "1181204bc0ef21c53bdd85ad29782e62",
+    "url": "/maxfarseer2/static/media/circles.1181204b.svg"
+  }
+]);
